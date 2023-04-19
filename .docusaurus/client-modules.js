98313f40b7557783fe61071db8ce8e357aa37a5f
Harish Mohan Raj <harish@airt.ai>
2023-04-19 12:21:46 +0530

    Initial commit

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/harishm/Dev/airt-git/tets/docusaurus-versioning-tutorial/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/harishm/Dev/airt-git/tets/docusaurus-versioning-tutorial/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/harishm/Dev/airt-git/tets/docusaurus-versioning-tutorial/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/harishm/Dev/airt-git/tets/docusaurus-versioning-tutorial/src/css/custom.css'),
+];
